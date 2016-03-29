0961f2a9af0f1ef667ce66ad8ff11fd7ee6f8907
parsa13881 <mohammadpa87@gmail.com>
2016-03-29 17:35:56 +0430

    Update wellcome.lua

--- plugins/wellcome.lua
+++ plugins/wellcome.lua
@@ -58,7 +58,7 @@ local function description_rules(msg, nama)
          rules = data[tostring(msg.to.id)]["rules"]
          rules = "\n› قوانین گروه :\n"..rules.."\n"
       end
-       local sambutan = "سلام "..nama.." عزیز\nبه گروه "..string.gsub(msg.to.print_name, "_", " ").."\nخوش آمدی \n بابا های من حسین پارسا اند \n خوش باش"
+       local sambutan = "سلام "..nama.." عزیز\nبه گروه "..string.gsub(msg.to.print_name, "_", " ").."\noخوش امدی \nبابای من پارسا هست \n خوش باش"
       local text = sambutan..about..rules.."\n"
       local receiver = get_receiver(msg)
       send_large_msg(receiver, text, ok_cb, false)
